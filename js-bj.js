da69a35b14963473d8d16e0851625870a7b1af69
Marysia-k09 <mariakochaj26@gmail.com>
2026-03-31 18:37:47 +0200

    Add files via upload

--- js-bj.js
+++ js-bj.js
@@ -1,19 +1,20 @@
-leet wyniki={
-wiosna_jasna;
-wiosna_ciepla;
-wiosna_czysta;
+let wyniki = {
+wiosna_jasna:0,
+wiosna_ciepla:0,
+wiosna_czysta:0,
 
-lato_jasne;
-lato_chlodne;
-lato_zgaszone;
+lato_jasne:0,
+lato_chlodne:0,
+lato_zgaszone:0,
 
-jesien_zgaszona;
-jesien_ciepla;
-jesien_ciemna;
+jesien_zgaszona:0,
+jesien_ciepla:0,
+jesien_ciemna:0,
 
-zima_czysta;
-zima_chlodna;
-zima_ciemna;}
+zima_czysta:0,
+zima_chlodna:0,
+zima_ciemna:0
+};
 
 const pytania = ["hair_child", "chai_now", "hair_light", "hair_sun", "hair_reflections", "eyes_color",
     "eyes_intensity", "eyes_extra", "eyes_ring", "eyes_type", "skin_color", "skin_sun",
@@ -22,3 +23,18 @@ const pytania = ["hair_child", "chai_now", "hair_light", "hair_sun", "hair_refle
     "nasycenie_urody", "oczy", "bialko", "uroda_bardziej", "twarz_bardziej", "charakter"
  ]
 
+const punkty = {
+    hair_child: {
+        bardzo_jasny_blond: {wiosna_jasna: 2, wiosna_czysta: 1, lato_jasne: 2, lato_chlodne: 1},
+        jasny_zloty_blond: {wiosna_jasna: 2, wiosna_ciepla: 2, wiosna_czysta: 1, jesien_ciepla: 1, jesien_zgaszona: 1},
+        jasny_popielaty_blond: {lato_jasne: 2, lato_chlodne: 2, lato_zgaszone: 1, zima_chlodna: 1},
+        ciemny_blond: {lato_chlodne: 1, lato_zgaszone: 2, jesien_zgaszona: 2, jesien_ciepla: 1},
+        jasny_braz: {wiosna_ciepla: 1, lato_chlodne: 1, lato_zgaszone: 2, jesien_zgaszona: 2, jesien_ciepla: 2, jesien_ciemna:1},
+        sredni_braz: {wiosna_ciepla: 1, lato_zgaszone: 1, jesien_zgaszona: 2, jesien_ciepla: 2, jesien_ciemna: 2, zima_ciemna:1},
+        ciemny_braz: {lato_zgaszone: 1, jesien_zgaszona: 1, jesien_ciepla: 1, jesien_ciemna: 2, zima_chlodna: 1, zima_ciemna: 2},
+        bardzo_ciemny_braz: {jesien_zgaszona: 1, jesien_ciepla: 1, jesien_ciemna: 2, zima_chlodna: 1, zima_ciemna: 2},
+        czarny: {jesien_zgaszona: 1, jesien_ciemna: 1, zima_chlodna: 1, zima_ciemna: 2},
+        rudy: {wiosna_jasna: 1, wiosna_ciepla: 1, jesien_zgaszona: 2, jesien_ciepla: 2, jesien_ciemna: 1},
+        miedziany: {wiosna_jasna: 1, wiosna_ciepla:2, jesien_zgaszona: 1, jesien_ciepla: 2, jesien_ciemna:1}
+    }
+}
